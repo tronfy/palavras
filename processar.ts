173acda15421d3e75e99255df9f1e3716b38af2e
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2022-07-25 02:06:59 -0300

    sanitizar, gerar slices

---
+++ processar.ts
@@ -0,0 +1,48 @@
+import fs from 'fs'
+
+const random = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min + 1)) + min
+
+const raw = fs.readFileSync('./palavras.txt', 'utf8').split('\n')
+
+const palavras: { [key: string]: { orig: string } } = {}
+
+raw
+  .filter(p => p.length >= 2 && !p.match(/.*[-\. ].*/))
+  .forEach(
+    p =>
+      (palavras[
+        p
+          .toLowerCase()
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .split('')
+          .filter(c => 'a' <= c && c <= 'z')
+          .join('')
+      ] = {
+        orig: p.toLowerCase(),
+      })
+  )
+
+fs.writeFileSync('./palavras.json', JSON.stringify(palavras, null, 2))
+
+const letras = 'abcdefghijklmnopqrstuvwxyz'
+let slices: { [key: string]: number } = {}
+
+for (let i = 0; i < letras.length; i++) {
+  for (let j = 0; j < letras.length; j++) {
+    const slice = letras[i] + letras[j]
+    const count = Object.keys(palavras).filter(palavra =>
+      palavra.includes(slice)
+    ).length
+
+    console.log(slice, count)
+    slices[slice] = count
+  }
+}
+
+slices = Object.fromEntries(
+  Object.entries(slices).sort(([, b], [, a]) => a - b)
+)
+
+fs.writeFileSync('./slices.json', JSON.stringify(slices, null, 2))
